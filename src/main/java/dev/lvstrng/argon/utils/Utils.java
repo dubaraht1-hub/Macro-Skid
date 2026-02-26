8f6c1b2b8b8f2a902ffc3f1727ae5ced4a9bb23c
dubaraht1-hub <dubaraht1@gmail.com>
2026-02-27 02:00:35 +0500

    Update Utils.java

--- src/main/java/dev/lvstrng/argon/utils/Utils.java
+++ src/main/java/dev/lvstrng/argon/utils/Utils.java
@@ -37,6 +37,46 @@ public final class Utils {
 		if (playerListEntry == null) return 0;
 		return playerListEntry.getLatency();
 	}
+}
+package dev.lvstrng.argon.utils;
+
+import dev.lvstrng.argon.module.modules.client.ClickGUI;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.entity.Entity;
+
+import java.awt.*;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import static dev.lvstrng.argon.Argon.mc;
+
+public final class Utils {
+
+	public static Color getMainColor(int alpha, int increment) {
+		int red = ClickGUI.red.getValueInt();
+		int green = ClickGUI.green.getValueInt();
+		int blue = ClickGUI.blue.getValueInt();
+
+		if (ClickGUI.rainbow.getValue()) {
+			return ColorUtils.getBreathingRGBColor(increment, alpha);
+		} else {
+			if (ClickGUI.breathing.getValue()) {
+				return ColorUtils.getMainColor(new Color(red, green, blue, alpha), increment, 20);
+			} else {
+				return new Color(red, green, blue, alpha);
+			}
+		}
+	}
+
+	public static int getPing(Entity player) {
+		if (mc.getNetworkHandler().getConnection() == null) return 0;
+
+		PlayerListEntry playerListEntry = mc.getNetworkHandler().getPlayerListEntry((player.getUuid()));
+		if (playerListEntry == null) return 0;
+		return playerListEntry.getLatency();
+	}
 
 	public static File getCurrentJarPath() throws URISyntaxException {
 		return new File(SelfDestruct.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath());
