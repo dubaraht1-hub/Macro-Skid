3a1a959e13976c6ee321e1862b3f2fe5df18bbb3
dubaraht1-hub <dubaraht1@gmail.com>
2026-02-27 02:06:36 +0500

    Update ModuleManager.java

--- src/main/java/dev/lvstrng/argon/module/ModuleManager.java
+++ src/main/java/dev/lvstrng/argon/module/ModuleManager.java
@@ -14,10 +14,7 @@ public final class ModuleManager implements ButtonListener {
     }
 
     public void addModules() {
-        // Client
         add(new ClickGUI());
-        
-        // Combat
         add(new AutoPotRefill());
     }
 
@@ -29,7 +26,6 @@ public final class ModuleManager implements ButtonListener {
         return modules;
     }
 
-    // This method is required by the GUI to function
     public List<Module> getModulesInCategory(Category category) {
         List<Module> categoryModules = new ArrayList<>();
         for (Module module : modules) {
@@ -40,35 +36,3 @@ public final class ModuleManager implements ButtonListener {
         return categoryModules;
     }
 }
-package dev.lvstrng.argon.module;
-
-import dev.lvstrng.argon.event.events.ButtonListener;
-import dev.lvstrng.argon.module.modules.combat.*;
-import dev.lvstrng.argon.module.modules.client.*;
-import java.util.ArrayList;
-import java.util.List;
-
-public final class ModuleManager implements ButtonListener {
-    private final List<Module> modules = new ArrayList<>();
-
-    public ModuleManager() {
-        addModules();
-    }
-
-    public void addModules() {
-        // Client
-        add(new ClickGUI());
-        
-        // Combat
-        add(new AutoPotRefill());
-    }
-
-    public void add(Module module) {
-        modules.add(module);
-    }
-
-    public List<Module> getModules() {
-        return modules;
-    }
-}
-
