2719b4540b90cbdc42535fb0b1354bc1244a4878
dubaraht1-hub <dubaraht1@gmail.com>
2026-02-27 14:28:43 +0500

    Update ModuleManager(1).java

--- src/main/java/dev/lvstrng/argon/module/ModuleManager(1).java
+++ src/main/java/dev/lvstrng/argon/module/ModuleManager(1).java
@@ -3,11 +3,7 @@ package dev.lvstrng.argon.module;
 import dev.lvstrng.argon.Argon;
 import dev.lvstrng.argon.event.events.ButtonListener;
 import dev.lvstrng.argon.module.modules.client.ClickGUI;
-import dev.lvstrng.argon.module.modules.client.Friends;
-import dev.lvstrng.argon.module.modules.client.SelfDestruct;
 import dev.lvstrng.argon.module.modules.combat.*;
-import dev.lvstrng.argon.module.modules.misc.*;
-import dev.lvstrng.argon.module.modules.render.*;
 import dev.lvstrng.argon.module.setting.KeybindSetting;
 import dev.lvstrng.argon.utils.EncryptedString;
 
@@ -26,48 +22,10 @@ public final class ModuleManager implements ButtonListener {
 
 	public void addModules() {
 		//Combat
-		add(new AimAssist());
-		add(new AnchorMacro());
-		add(new AutoCrystal());
-		add(new AutoDoubleHand());
-		add(new AutoHitCrystal());
-		add(new AutoInventoryTotem());
-		add(new TriggerBot());
-		add(new AutoPot());
 		add(new AutoPotRefill());
-		add(new AutoWTap());
-		add(new CrystalOptimizer());
-		add(new DoubleAnchor());
-		add(new HoverTotem());
-		add(new NoMissDelay());
-		add(new ShieldDisabler());
-		add(new TotemOffhand());
-		add(new AutoJumpReset());
-
-		//Misc
-		add(new Prevent());
-		add(new AutoXP());
-		add(new NoJumpDelay());
-		add(new PingSpoof());
-		add(new FakeLag());
-		add(new AutoClicker());
-		add(new KeyPearl());
-		add(new NoBreakDelay());
-		add(new Freecam());
-		add(new PackSpoof());
-		add(new Sprint());
-
-		//Render
-		add(new HUD());
-		add(new NoBounce());
-		add(new PlayerESP());
-		add(new StorageEsp());
-		add(new TargetHud());
 
 		//Client
 		add(new ClickGUI());
-		add(new Friends());
-		add(new SelfDestruct());
 	}
 
 	public List<Module> getEnabledModules() {
