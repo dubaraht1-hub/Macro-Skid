b171571abca238e8abefb081bd30fd7adf6b2ba7
dubaraht1-hub <dubaraht1@gmail.com>
2026-03-01 21:13:44 +0500

    Update Category.java

--- src/main/java/dev/lvstrng/argon/module/Category.java
+++ src/main/java/dev/lvstrng/argon/module/Category.java
@@ -3,7 +3,7 @@ package dev.lvstrng.argon.module;
 import dev.lvstrng.argon.utils.EncryptedString;
 
 public enum Category {
-	COMBAT(EncryptedString.of("Combat")),
+	COMBAT(EncryptedString.of("Nethpot")),
 	CLIENT(EncryptedString.of("Client"));
 	public final CharSequence name;
 
